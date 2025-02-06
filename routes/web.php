2a88ade5fa3ef58a080f3b6f8174879d9e1de265
Faraz <farazpourebrahimh@gmail.com>
2025-02-06 21:14:09 +0330

    [ADD] home and job controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('jobs', [JobController::class,"index"]);
