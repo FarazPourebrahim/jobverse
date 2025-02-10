f8364a233cce2d672eeee3c2bfbd90fa91c1b0b3
Faraz <farazpourebrahimh@gmail.com>
2025-02-10 22:51:48 +0330

    [ADD] alpine js to handle js

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,7 +1,7 @@
 @props(['type', 'message'])
 
-@if(session()->has('success') || session()->has('error'))
-    <div class="p-4 mb-4 text-sm text-white {{ $type === 'success' ? 'bg-green-500' : 'bg-red-500' }} rounded">
+@if(session()->has($type))
+    <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 5000)" x-show="show" class="p-4 mb-4 text-sm text-white {{ $type === 'success' ? 'bg-green-500' : 'bg-red-500' }} rounded">
         {{ $message }}
     </div>
 @endif
