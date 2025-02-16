16784e6ae514e356a8162464cd53aafa6bf6c5b9
Faraz <farazpourebrahimh@gmail.com>
2025-02-16 20:44:18 +0330

    [ADD] job application

---
+++ app/Http/Controllers/ApplicantController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Applicant;
+use App\Models\Job;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class ApplicantController extends Controller
+{
+    public function store(Request $request, Job $job): RedirectResponse {
+
+        $sxistingApplication = Applicant::where('job_id', $job->id)->where('user_id', auth()->id())->exists();
+        if ($sxistingApplication) {
+            return redirect()->back()->with('error', 'You have already applied for this job');
+        }
+
+        $validatedData = $request->validate([
+           'full_name' => 'required|string|max:255',
+            'contact_phone' => 'string',
+            'contact_email' => 'required|string|email',
+            'message' => 'string',
+            'location' => 'string',
+            'resume' => 'required|file|mimes:pdf|max:2048',
+        ]);
+
+        if ($request->hasFile('resume')) {
+            $path = $request->file('resume')->store('resumes', 'public');
+            $validatedData['resume_path'] = $path;
+        }
+
+        $application = new Applicant($validatedData);
+        $application->job_id = $job->id;
+        $application->user_id = auth()->id();
+        $application->save();
+
+        return redirect()->back()->with('success', 'Your application has been submitted!');
+    }
+
+    public function destroy(Request $request, Applicant $applicant): RedirectResponse
+    {
+        if ($applicant->user->id === auth()->id()) {
+            $applicant->delete();
+            return redirect()->route('dashboard')->with('success', 'Applicant has been deleted successfully!');
+        }
+
+        return redirect()->route('dashboard')->with('error', 'You are not allowed to delete this applicant!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ApplicantController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
@@ -20,6 +21,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/bookmarks', [BookmarkController::class, 'index'])->name('bookmarks.index');
     Route::post('/bookmarks{job}', [BookmarkController::class, 'store'])->name('bookmarks.store');
     Route::delete('/bookmarks{job}', [BookmarkController::class, 'destroy'])->name('bookmarks.destroy');
+    Route::post('/jobs/{job}/apply', [ApplicantController::class, 'store'])->name('applicant.store');
+    Route::delete('/applicants/{applicant}', [ApplicantController::class, 'destroy'])->name('applicant.destroy');
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 });
 
